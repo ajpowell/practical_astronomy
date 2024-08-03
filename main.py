68700ad635f3c0b6bb65406573a4d50736b187e4
= <adrian.powell@apmf.co.uk>
2024-08-03 23:33:57 +0100

    Updated to show today's times

--- main.py
+++ main.py
@@ -26,7 +26,17 @@ def main():
     lat = 51.860435
     long = -1.596303
 
+    print('{}-{:02d}-{:02d}'.format(year, month, day))
     astro_sun.sunRiseSet(year, month, day, lat, long)
+
+    print('')
+
+    today_year = datetime.datetime.now().year
+    today_month = datetime.datetime.now().month
+    today_day = datetime.datetime.now().day
+
+    print('{}-{:02d}-{:02d}'.format(today_year, today_month, today_day))
+    astro_sun.sunRiseSet(today_year, today_month, today_day, lat, long) 
 
 
 if __name__ == "__main__":
